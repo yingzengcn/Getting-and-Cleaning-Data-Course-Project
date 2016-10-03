1a7a7855308310597b1636c10d1dfd8acdf6061e
Ying <yingzengcn@gmail.com>
2016-10-03 14:29:42 +0800

    code for final project

---
+++ final project.R
@@ -0,0 +1,42 @@
+
+1. 
+if(!file.exists("./data")) dir.create("./data")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./data/getCleanData.zip")
+
+2. 
+listZip <- unzip("./data/getCleanData.zip", exdir = "./data")
+
+3. 
+train.x <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+train.y <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+train.subject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+test.x <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+test.y <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+test.subject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+trainData <- cbind(train.subject, train.y, train.x)
+testData <- cbind(test.subject, test.y, test.x)
+fullData <- rbind(trainData, testData)
+
+
+4.
+featureName <- read.table("./data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[,2]
+featureIndex <- grep(("mean\\(\\)|std\\(\\)"), featureName)
+finalData <- fullData[, c(1, 2, featureIndex+2)]
+colnames(finalData) <- c("subject", "activity", featureName[featureIndex])
+
+activityName <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+finalData$activity <- factor(finalData$activity, levels = activityName[,1], labels = activityName[,2])
+names(finalData) <- gsub("\\()", "", names(finalData))
+names(finalData) <- gsub("^t", "time", names(finalData))
+names(finalData) <- gsub("^f", "frequence", names(finalData))
+names(finalData) <- gsub("-mean", "Mean", names(finalData))
+names(finalData) <- gsub("-std", "Std", names(finalData))
+
+library(dplyr)
+groupData <- finalData %>%
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
+
+write.table(groupData, file ="./data/MeanData.txt", row.names = FALSE)
+
